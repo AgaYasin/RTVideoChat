71bc6827ea1792895c2cd21bce74dfac6f3e7d03
AgaYasin <yasin.kfl@gmail.com>
2023-01-09 05:36:17 +0300

    Final working version

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -52,15 +52,40 @@ const Options = ({children}) => {
                             <TextField label='name' value={name} onChange={(e) => setName(e.target.value)} fullWidth/>
                             <CopyToClipboard text={me} className={classes.margin}>
                                 <Button variant='contained' color='primary' fullWidth startIcon={<Assignment fontSize='large'/>}>
-                                    
+                                    Copy your ID
                                 </Button>
                             </CopyToClipboard>
                         </Grid>
+                        <Grid item xs={12} md={6} className={classes.padding}>
+                            <Typography gutterBottom variant='h6'>Make a call</Typography>
+                            <TextField label="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth/>
+                            {callAccepted && !callEnded ? (
+                              <Button variant="contained" 
+                              color="secondary" 
+                              startIcon={<PhoneDisabled fontSize="large"/>}
+                              fullWidth
+                              onClick={leaveCall}
+                              className={classes.margin}>
+                                Hang up
+                              </Button>
+                            ):(
+                              <Button variant="contained" 
+                              color="primary" 
+                              startIcon={<Phone fontSize="large"/>}
+                              fullWidth
+                              onClick={() => callUser(idToCall)}
+                              className={classes.margin}>
+                                Call
+                              </Button>
+
+                            )}
+                        </Grid>
                     </Grid>
                 </form>
+                {children}
             </Paper>
-            Options
-            {children}
+           
+            
         </Container>
     )
 }
